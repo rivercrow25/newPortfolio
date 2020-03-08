ae751e3c0d0c458484549613582348d178dc984b
nick migel <nickmigel2@gmail.com>
2020-03-08 14:10:32 -0400

    added a new project

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       {Project('Save the animals!', 'HTML', 'CSS', 'Created a marketing page', 'A marketing website built with html and css for my first build week at lamda', 'https://github.com/BW-SaveTheAnimalsTeam3/marketing-page', 'https://savetheanimalsweb28.netlify.com')}
       {Project('Brick break', 'JavaScript', 'Html', 'Proving to myself lamda was a good choice', 'I used this game to try and teach myself es6 syntax before i found lamda and failed, I attempted this again after attending lamda for 3 weeks', 'https://github.com/rivercrow25/randomGame', 'https://one-off-brickbreakgame.netlify.com')}
       {Project('Nasa Api', 'React', 'ReactStrap', 'javascript', 'this is a project we worked on durring class to learn react app and advanced styling techniques', 'https://github.com/rivercrow25/nasa-photo-of-the-day', 'https://nasa-photo-of-the-dayweb28.netlify.com')}
+      {Project('Spotifind', 'React', 'Redux', 'CSS', 'A build week project that uses the spotify api to allow users to find songs based on any song you pick from a list of 70K', 'https://github.com/Build-Week-Spotify-Song-Suggester-5/Front-End-Dev', 'https://spotifind.now.sh/')}
     </div>
   );
 }
