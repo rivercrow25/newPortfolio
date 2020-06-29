b18ba0e5ca0e23a2b2c334eb1e206be2b9bc7ab2
nick migel <nickmigel2@gmail.com>
2020-06-29 13:28:43 -0400

    updated my projects and removed useless navbar

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
       <Header />
-      {Section('Blog', 'I will be trying to keep up with a blog', 'Visit my blog page')}
-      {Section('Resume', 'I dont have one yet :D', 'visit this empty page anyway')}
-      {Section('Contact', 'You can find me on slack or email me', 'Visit this page to view my contact details')}
+      {Section(<a href='https://docs.google.com/document/d/1wsyyb04JRT1eC4xdZstSQm4kq4hipncNBXP3GMqy1vo/edit?usp=sharing'>View my resume</a>)}
+      {Section('Contact', 'my email is nickmigel2@gmail.com')}
       {/* {Project('Save the animals!', 'build Week project 1', 'A marketing website built with html and css for my first build week at lamda', 'https://github.com/BW-SaveTheAnimalsTeam3/marketing-page', 'https://savetheanimalsweb28.netlify.com')}
       {Project('Brick break', 'My first game', 'I used this game to try and teach myself es6 syntax before i found lamda and failed, I attempted this again after attending lamda for 3 weeks', 'https://github.com/rivercrow25/randomGame', 'https://one-off-brickbreakgame.netlify.com')}
       {Project('Nasa Api', 'Two day project', 'this is a project we worked on durring class to learn react app and advanced styling techniques', 'https://github.com/rivercrow25/nasa-photo-of-the-day', 'https://nasa-photo-of-the-dayweb28.netlify.com')} */}
-      {Project('Save the animals!', 'HTML', 'CSS', 'Created a marketing page', 'A marketing website built with html and css for my first build week at lamda', 'https://github.com/BW-SaveTheAnimalsTeam3/marketing-page', 'https://savetheanimalsweb28.netlify.com')}
-      {Project('Brick break', 'JavaScript', 'Html', 'Proving to myself lamda was a good choice', 'I used this game to try and teach myself es6 syntax before i found lamda and failed, I attempted this again after attending lamda for 3 weeks', 'https://github.com/rivercrow25/randomGame', 'https://one-off-brickbreakgame.netlify.com')}
+      {Project('Data driven transit!', 'React', 'SCSS', 'A single page application widget', 'Saves historical data in San Francisco to help SFMTA make informed descisions to better the transit systems', 'https://github.com/Lambda-School-Labs/sfmta-data-analysis-fe/tree/master/src', 'https://www.datadriventransit.org/')}
+      {Project('Brick break', 'JavaScript', 'HTML', 'Proving to myself lamda was a good choice', 'I used this game to try and teach myself es6 syntax before i found lamda and failed, I attempted this again after attending lamda for 3 weeks', 'https://github.com/rivercrow25/randomGame', 'https://one-off-brickbreakgame.netlify.com')}
       {Project('Nasa Api', 'React', 'ReactStrap', 'javascript', 'this is a project we worked on durring class to learn react app and advanced styling techniques', 'https://github.com/rivercrow25/nasa-photo-of-the-day', 'https://nasa-photo-of-the-dayweb28.netlify.com')}
       {Project('Spotifind', 'React', 'Redux', 'CSS', 'A build week project that uses the spotify api to allow users to find songs based on any song you pick from a list of 70K', 'https://github.com/Build-Week-Spotify-Song-Suggester-5/Front-End-Dev', 'https://spotifind.now.sh/')}
     </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
         <Jumbotron>
             <img src='https://ca.slack-edge.com/T4JUEB3ME-US8LU3MRV-a9e330fe46d7-512' alt='picture of Nick Migel' />
             <h1 className="display-3">Nick Migel</h1>
-            <p className="lead">Web Dev with a bad hobby of playing videos games</p>
+            <p className="lead">Full Stack Web Developer studying at Lambda school currently learning computer science</p>
             <hr className="my-2" />
-            <p>Languages: Html, Css, Javascript</p>
+            <p>Languages: HTML, CSS, Javascript, React, Redux, Node.js, Express</p>
             <p className="lead">
-                Mission: Here to chew gum and learn to code and I'm all out of gum
+                Mission: Hard worker with a passion for coding in general
             </p>
         </Jumbotron>
     )
